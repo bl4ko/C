4b3da58810bac2af078668aace300e61e4db86a4
Bl4ko <gasperoblakk@outlook.com>
2021-04-30 18:38:13 +0200

    vaje 10

--- lab_works/vaje10/naloga.c
+++ lab_works/vaje10/naloga.c
@@ -6,28 +6,59 @@
 
 #include "naloga.h"
 
+//vsota iterativno
 int vsotaI(Vozlisce* zacetek) {
-    // popravite / dopolnite ...
-    return -999;
+    int vsota = 0;
+    while (zacetek != NULL) {
+        vsota += zacetek->podatek;
+        zacetek = zacetek->naslednje;
+    }
+    return vsota;
 }
 
 int vsotaR(Vozlisce* zacetek) {
-    // popravite / dopolnite ...
-    return -999;
+    if (zacetek == NULL)
+        return 0;
+    return zacetek->podatek + vsotaR(zacetek->naslednje);
 }
 
 Vozlisce* vstaviUrejenoI(Vozlisce* zacetek, int element) {
-    // popravite / dopolnite ...
-    return NULL;
+    Vozlisce* trenutno = zacetek;
+    Vozlisce* novo = malloc(sizeof(Vozlisce));
+    novo->podatek=element;
+    if (trenutno == NULL || trenutno->podatek > element) {
+        novo->naslednje=trenutno;
+        return novo;
+    }
+    while (trenutno->naslednje != NULL && trenutno->naslednje->podatek < element) {
+        trenutno = trenutno -> naslednje;
+    }
+    novo->naslednje = trenutno->naslednje;
+    trenutno->naslednje = novo;
+    return zacetek;
 }
 
+
+
 Vozlisce* vstaviUrejenoR(Vozlisce* zacetek, int element) {
-    // popravite / dopolnite ...
-    return NULL;
+    if (zacetek == NULL || zacetek->podatek >= element) {
+        Vozlisce* novo = malloc(sizeof(Vozlisce));
+        novo -> naslednje = zacetek;
+        novo -> podatek = element;
+        return novo;
+    }
+    else if (zacetek->naslednje == NULL || zacetek->naslednje->podatek >= element) {
+        Vozlisce* novo = malloc(sizeof(Vozlisce));
+        novo->podatek = element;
+        novo->naslednje = zacetek->naslednje;
+        zacetek->naslednje = novo;
+    }
+    else {
+        vstaviUrejenoR(zacetek->naslednje, element);
+    }
+    return zacetek;
 }
 
 int main() {
-    // koda za ro"cno testiranje (po "zelji)
-
     return 0;
 }
