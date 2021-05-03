4dbdd6dfec6eaff49a523367784e6b303471189a
Bl4ko <gasperoblakk@outlook.com>
2021-05-03 17:57:29 +0200

    vaje 10
    narejene nove vaje

--- lab_works/vaje10/naloga.c
+++ lab_works/vaje10/naloga.c
@@ -6,7 +6,6 @@
 
 #include "naloga.h"
 
-//vsota iterativno
 int vsotaI(Vozlisce* zacetek) {
     int vsota = 0;
     while (zacetek != NULL) {
@@ -23,39 +22,30 @@ int vsotaR(Vozlisce* zacetek) {
 }
 
 Vozlisce* vstaviUrejenoI(Vozlisce* zacetek, int element) {
-    Vozlisce* trenutno = zacetek;
     Vozlisce* novo = malloc(sizeof(Vozlisce));
     novo->podatek=element;
-    if (trenutno == NULL || trenutno->podatek > element) {
-        novo->naslednje=trenutno;
-        return novo;
+    if (zacetek == NULL || zacetek->podatek > element) {
+        novo->naslednje=zacetek;
+        zacetek = novo;
+    } else {
+        Vozlisce* v = zacetek;
+        while (v->naslednje != NULL && v->naslednje->podatek < element) {
+            v = v -> naslednje;
+        }
+        novo -> naslednje = v->naslednje;
+        v -> naslednje = novo;
     }
-    while (trenutno->naslednje != NULL && trenutno->naslednje->podatek < element) {
-        trenutno = trenutno -> naslednje;
-    }
-    novo->naslednje = trenutno->naslednje;
-    trenutno->naslednje = novo;
     return zacetek;
 }
 
-
-
 Vozlisce* vstaviUrejenoR(Vozlisce* zacetek, int element) {
-    if (zacetek == NULL || zacetek->podatek >= element) {
-        Vozlisce* novo = malloc(sizeof(Vozlisce));
+    if (zacetek == NULL || element < zacetek -> podatek) {
+        Vozlisce* novo = malloc (sizeof(Vozlisce));
         novo -> naslednje = zacetek;
         novo -> podatek = element;
         return novo;
     }
-    else if (zacetek->naslednje == NULL || zacetek->naslednje->podatek >= element) {
-        Vozlisce* novo = malloc(sizeof(Vozlisce));
-        novo->podatek = element;
-        novo->naslednje = zacetek->naslednje;
-        zacetek->naslednje = novo;
-    }
-    else {
-        vstaviUrejenoR(zacetek->naslednje, element);
-    }
+    zacetek -> naslednje = vstaviUrejenoR(zacetek -> naslednje, element);
     return zacetek;
 }
 
